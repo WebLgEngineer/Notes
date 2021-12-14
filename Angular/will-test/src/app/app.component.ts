8f886cc2c33708d2bb749e1aea0fda3388fa1659
Amber_lo <79530642+WebLgEngineer@users.noreply.github.com>
2021-12-14 10:03:01 +0800

    12/13  module&component 解析

--- Angular/will-test/src/app/app.component.ts
+++ Angular/will-test/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+@Component({ // 用來宣告這個class 是一個component
+  selector: 'app-root', // 選取器(對應到html標籤，也可以使用.app-root 則html上要寫 <div class="app-root"></div>)
+  templateUrl: './app.component.html', // 對應AppComponent的html
+  styleUrls: ['./app.component.scss'] // 對應AppComponent的css
 })
 export class AppComponent {
   title = 'will-test';
--- Angular/will-test/src/app/app.module.ts
+++ Angular/will-test/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
+  declarations: [ // 用來宣告跟view有關的元件
     AppComponent
   ],
-  imports: [
+  imports: [ // 用來匯入這個模組會用到的其他模組(模組及是元件集合)
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // 註冊一些服務的提供者
+  bootstrap: [AppComponent] // 啟動根元件
 })
 export class AppModule { }
